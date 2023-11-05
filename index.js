cbdda16c9a7bdbb18544c8294fc74af44bf388c6
mrana007 <rana.es.bc@gmail.com>
2023-11-05 17:30:29 +0600

    create assignment collection in mongodb to colllect assignment data from client side

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kezvwg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,12 +25,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const assignmentCollection = client.db('groupStudyDB').collection('assignment')
+
+    // receive assignment data server from client
+    app.post("/assignments", async(req, res) =>{
+        const newAssignment = req.body;
+        console.log(newAssignment);
+        const result = await assignmentCollection.insertOne(newAssignment);
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
